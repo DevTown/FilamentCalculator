ad202b13723384b3ce5b5c8b8fe66024acd2dcbf
DevTown <marv@dev-town.de>
2025-07-01 23:30:59 +0200

    Add FilamentType Edit, Glowup Butons, Add Shipmentcosts

---
+++ src/FilamentCalcWeb/ViewModels/FilamentTypeViewModel.cs
@@ -0,0 +1,8 @@
+namespace FilamentCalculator.ViewModels;
+
+public class FilamentTypeViewModel
+{
+    public int FilamentTypeId { get; set; }
+    public string Name { get; set; }
+    public bool IsInUse { get; set; }
+}
